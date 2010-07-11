066b92f6eae0b8fc3a439d96f848e4463621f3df
Christian Jaeger <chrjae@gmail.com>
2010-07-11 15:15:13 -0400

    move settings out to example/my-service-settings.sh, wrapper to use it
    daemon and sender are still hardcoded

---
+++ example/my-service-settings.sh
@@ -0,0 +1,14 @@
+export SERVICE_NAME=my-service
+export SERVICE_RUN_DIR=/var/run
+
+#export SERVICE_INSTALLATION_DIR=/opt/my-daemon/
+
+# SERVICE_DAEMON is being fed to system, thus can contain arguments
+# (it's split on spaces etc.)
+
+export SERVICE_DAEMON=/opt/my-daemon/daemon
+
+
+#pidfile="/var/run/$servicename.pid";
+#lockfile="/var/run/$servicename.lck";
+
