ac52e688c21754e4ad807fa2632acf4674de4712
Christian Jaeger <chrjae@gmail.com>
2010-07-11 15:57:23 -0400

    finish separating configuration out of startstop and daemon

--- example/my-service-settings.sh
+++ example/my-service-settings.sh
@@ -1,14 +1,15 @@
+# For simplicity, using the same settings file for both startstop and
+# the actual daemon.
+
+# startstop settings:
+
 export SERVICE_NAME=my-service
 export SERVICE_RUN_DIR=/var/run
 
-#export SERVICE_INSTALLATION_DIR=/opt/my-daemon/
-
 # SERVICE_DAEMON is being fed to system, thus can contain arguments
 # (it's split on spaces etc.)
-
 export SERVICE_DAEMON=/opt/my-daemon/daemon
 
+# daemon settings:
 
-#pidfile="/var/run/$servicename.pid";
-#lockfile="/var/run/$servicename.lck";
-
+export DAEMON_FIFO=/var/run/"$SERVICE_NAME".fifo
