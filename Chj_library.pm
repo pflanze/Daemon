0cf85b991a2ed49ddc232bc59e4d8fc7f1d56c11
Christian Jaeger <chrjae@gmail.com>
2010-07-11 20:45:33 -0400

    move library code to Chj_library

---
+++ Chj_library.pm
@@ -0,0 +1,67 @@
+
+=head1 NAME
+
+Chj_library
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+Library for Daemon package, mostly put together from Chj:: modules.
+
+=cut
+
+# for simplicity, just use the main namespace to avoid having to export stuff.
+#package Chj_library;
+#@ISA="Exporter"; require Exporter;
+#@EXPORT=qw();
+#@EXPORT_OK=qw();
+#%EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
+
+use strict;
+
+sub msg {
+    print @_,"\n"
+}
+
+sub xenv {
+    my ($key)=@_;
+    my $v= $ENV{$key};
+    defined $v
+      or die "missing environment variable '$key'";
+    $v
+}
+
+sub xfork {
+    my $pid=fork;
+    defined $pid or die "fork: $!";
+    $pid
+}
+
+sub xexec {
+    exec @_
+      or exit 127;
+}
+
+sub xclose {
+    my ($fh)=@_;
+    close $fh
+      or die "close $fh: $!";
+}
+
+sub xdup2 {
+    my $self=shift;
+    my $myfileno= CORE::fileno $self;
+    defined $myfileno
+      or die;
+    require POSIX;
+    for my $dup (@_) {
+	my $fileno= $dup=~ /^\d+\z/s ? $dup : die;
+	POSIX::dup2($myfileno,$fileno)
+	  or die "xdup2 (fd $myfileno) to $dup (fd $fileno): $!";
+    }
+}
+
+
+
+1
