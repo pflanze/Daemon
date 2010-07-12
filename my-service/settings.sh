6407fa7682146fd233d472e1b9a88e0b36a9decd
Christian Jaeger <chrjae@gmail.com>
2010-07-12 01:58:31 -0400

    pass arguments as sh arguments!

--- my-service/settings.sh
+++ my-service/settings.sh
@@ -28,4 +28,4 @@ export DAEMON_SOCKET_GROUP=""
 # *a line of text*) is received over the fifo; the message being
 # offered on the program's stdin.
 # DAEMON_MAINPROGRAM will be passed to sh -c
-export DAEMON_MAINPROGRAM="/bin/echo"
+export DAEMON_MAINPROGRAM='perl -we '\''for(@ARGV){print "arg: [$_]\n"}'\'' a b c "$@" z'
